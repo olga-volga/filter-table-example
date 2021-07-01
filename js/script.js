166e21989676a899b62fc3192cc1e0f083cb1f72
olga <yaolmi@yandex.ru>
2021-07-01 15:47:39 +0700

    write js script

--- js/script.js
+++ js/script.js
@@ -2,6 +2,19 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
-	
+	const userInput = document.querySelector('.form__input'),
+		  name = document.querySelectorAll('[data-name]'),
+		  tableRow = document.querySelectorAll('.table__row');
 
+	function hideRow() {
+		name.forEach(item => {
+			if (userInput.value == '' || item.innerHTML.includes(userInput.value.toLowerCase())) {
+				item.parentElement.style.display = '';
+			} else {
+				item.parentElement.style.display = 'none';
+			}
+		});
+	}
+	
+	userInput.addEventListener('input', hideRow); 
 });
